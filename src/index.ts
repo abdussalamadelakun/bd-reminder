92f266ab8e945e2e09a6743649b5c64a683c3746
MacBook Pro <apple@apples-MacBook-Pro.local>
2026-09-22 11:36:42 -0500

    Integrate cron with db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
-
+import pool from "./db.js";
 import cron from "node-cron";
 import express from "express";
+
 // import { fileURLToPath } from "url";
 // import { join, dirname } from "path";
 
@@ -42,22 +42,28 @@ const profiles = [
 //   }
 // })();
 
-cron.schedule("34 19 * * *", () => {
+cron.schedule("35 11 * * *", async () => {
   const currentDate = new Date();
+  const isoDateString = currentDate.toISOString().split("T")[0]?.substring(5);
+  const query = `SELECT * from profiles WHERE date_of_birth LIKE $1`;
+  const values = ["%" + isoDateString];
+  const result = await pool.query(query, values);
+  console.log(`Happy birthday, ${result.rows[0].full_name}`);
+
   console.log(currentDate);
 
-  for (const profile of profiles) {
-    // const birthMonth: string = profile.dateOfBirth.split("-")[1];
-    // const birthDay: string = profile.dateOfBirth.split("-")[2];
-    const birthMonth = profile.dateOfBirth.split("-")[1];
-    const birthDay = profile.dateOfBirth.split("-")[2];
-    if (
-      String(currentDate.getMonth() + 1).padStart(2, "0") == birthMonth &&
-      String(currentDate.getDate()).padStart(2, "0") == birthDay
-    ) {
-      console.log(`Happy birthday, ${profile.fullName}`);
-    }
-  }
+  // for (const profile of profiles) {
+  //   // const birthMonth: string = profile.dateOfBirth.split("-")[1];
+  //   // const birthDay: string = profile.dateOfBirth.split("-")[2];
+  //   const birthMonth = profile.dateOfBirth.split("-")[1];
+  //   const birthDay = profile.dateOfBirth.split("-")[2];
+  //   if (
+  //     String(currentDate.getMonth() + 1).padStart(2, "0") == birthMonth &&
+  //     String(currentDate.getDate()).padStart(2, "0") == birthDay
+  //   ) {
+  //     console.log(`Happy birthday, ${profile.fullName}`);
+  //   }
+  // }
 });
 
 app.listen(8080, () => {
